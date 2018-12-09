45a1bea702d918be9cbf71288bc79e78bd8d9810
Krzysztof Witalewski <krzysztofwitalewski@workfront.com>
2018-12-09 18:15:15 +0100

    reorder chart colors for greater clarity when possible

--- src/global/styleConstants.js
+++ src/global/styleConstants.js
@@ -4,8 +4,8 @@ export const sectionMargin = 16;
 export const chartBackground = '#eef7f4';
 export const chartColors = [
     '#5db990',
-    '#558ca0',
     '#864ba5',
-    '#829ac0',
     '#a7e17a',
+    '#558ca0',
+    '#829ac0',
 ];
