7097d999c7ff8f41b3d644bafb8a73b16686466d
Krzysztof Witalewski <krzysztofwitalewski@workfront.com>
2018-12-09 18:59:40 +0100

    make sure getLowerRangeSeriesKeys works for even numbers

--- src/utils/getLowerRangeSeriesKeys.test.js
+++ src/utils/getLowerRangeSeriesKeys.test.js
@@ -4,13 +4,22 @@ import { getLabelMap } from './getLabelMap';
 import { mapDataSetLabels } from './mapDataSetLabels';
 
 describe('getLowerRangeSeriesKeys', () => {
-  it('returns lower half of all series baser on average value', () => {
+  it('returns lower half of all series based on average value - for mock data', () => {
     const labelMap = getLabelMap(mockChartData.monthlySales);
     const processedData = mapDataSetLabels(
       labelMap,
       mockChartData.monthlySales.data
     );
     const result = getLowerRangeSeriesKeys(labelMap, processedData);
+    expect(result).toEqual(['Subscription', 'Single/Rental']);
+  });
+  it('returns lower half of all series based on average value - for modified mock data with even number of series', () => {
+    const labelMap = getLabelMap(mockChartData.monthlySales);
+    const processedData = mapDataSetLabels(
+      labelMap,
+      mockChartData.monthlySales.data
+    );
+    const result = getLowerRangeSeriesKeys(labelMap, processedData.slice(1));
     expect(result).toEqual(['Subscription', 'Single/Rental']);
   });
 });
