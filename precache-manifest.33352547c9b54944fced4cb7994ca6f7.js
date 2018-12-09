cbc9f127d54708677a5b74af97f2897bac9958e9
Krzysztof Witalewski <krzysztofwitalewski@workfront.com>
2018-12-09 15:56:22 +0100

    Updates

---
+++ precache-manifest.33352547c9b54944fced4cb7994ca6f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "81312d4a81e499095a3d",
+    "url": "/dashboard/static/js/runtime~main.81312d4a.js"
+  },
+  {
+    "revision": "7f286ab945d792e22ead",
+    "url": "/dashboard/static/js/main.7f286ab9.chunk.js"
+  },
+  {
+    "revision": "9c342b6044695d88ac29",
+    "url": "/dashboard/static/js/1.9c342b60.chunk.js"
+  },
+  {
+    "revision": "7f286ab945d792e22ead",
+    "url": "/dashboard/static/css/main.130d1c31.chunk.css"
+  },
+  {
+    "revision": "1aab6a466eb159b341e4f3812ccd3d0d",
+    "url": "/dashboard/index.html"
+  }
+];
