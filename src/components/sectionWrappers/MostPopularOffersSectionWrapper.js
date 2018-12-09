63eb893d7407b30bf5b4a5c728e28f5cf8ed19ae
Krzysztof Witalewski <krzysztofwitalewski@workfront.com>
2018-12-09 15:31:37 +0100

    add most popular offers chart

--- src/components/sectionWrappers/MostPopularOffersSectionWrapper.js
+++ src/components/sectionWrappers/MostPopularOffersSectionWrapper.js
@@ -1,20 +1,49 @@
 import React from 'react';
-import styled from '@emotion/styled';
+import { PieChart, Pie, Cell, Legend, Tooltip } from 'recharts';
+import { chartColors } from '../../global/styleConstants';
 import { Section } from '../Section';
 
-const StyledMostPopularOffersSectionWrapper = styled.div`
-  min-width: 400px;
-  .section-content {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
+function reformatLabels() {
+  const labels = document.querySelectorAll(
+    '.most-popular-offers .recharts-pie-label-text tspan'
+  );
+  for (let i = 0; i < labels.length; i++) {
+    labels[i].textContent = `${labels[i].textContent}%`;
   }
-`;
+}
 
-export const MostPopularOffersSectionWrapper = ({ className, data }) => (
-  <StyledMostPopularOffersSectionWrapper className={className}>
-    <Section title="Most popular offers" className="most-popular-offers">
-      <div className="section-content">SECTION CONTENT</div>
+export const MostPopularOffersSectionWrapper = ({ className, data }) => {
+  const sum = data.reduce((acc, { value }) => acc + value, 0);
+  const percentData = data.map(({ value, tooltip }) => ({
+    name: tooltip,
+    value: Math.round((value * 100) / sum),
+  }));
+  return (
+    <Section title="Most popular offers" className={className}>
+      <PieChart width={450} height={250}>
+        <Pie
+          data={percentData}
+          cx={120}
+          cy={120}
+          innerRadius={40}
+          outerRadius={80}
+          fill="#8884d8"
+          dataKey="value"
+          label
+          onAnimationEnd={reformatLabels}
+        >
+          {percentData.map(({ value }, index) => (
+            <Cell key={value} fill={chartColors[index % chartColors.length]} />
+          ))}
+        </Pie>
+        <Tooltip />
+        <Legend
+          width={200}
+          align="right"
+          layout="vertical"
+          wrapperStyle={{ top: 50, left: 250 }}
+        />
+      </PieChart>
     </Section>
-  </StyledMostPopularOffersSectionWrapper>
-);
+  );
+};
