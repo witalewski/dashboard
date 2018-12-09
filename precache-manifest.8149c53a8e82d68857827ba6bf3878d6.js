338e82964b7b393df7aa9279d5b54999b88180da
Krzysztof Witalewski <krzysztofwitalewski@workfront.com>
2018-12-09 17:58:50 +0100

    Updates

---
+++ precache-manifest.8149c53a8e82d68857827ba6bf3878d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "81312d4a81e499095a3d",
+    "url": "/dashboard/static/js/runtime~main.81312d4a.js"
+  },
+  {
+    "revision": "49b45dfbf3a20071882c",
+    "url": "/dashboard/static/js/main.49b45dfb.chunk.js"
+  },
+  {
+    "revision": "f6be391e6c830582442a",
+    "url": "/dashboard/static/js/1.f6be391e.chunk.js"
+  },
+  {
+    "revision": "49b45dfbf3a20071882c",
+    "url": "/dashboard/static/css/main.33e815f4.chunk.css"
+  },
+  {
+    "revision": "05a424c0755a2282b8fb439fb23aeb38",
+    "url": "/dashboard/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/dashboard/precache-manifest.33352547c9b54944fced4cb7994ca6f7.js"
+  "/dashboard/precache-manifest.8149c53a8e82d68857827ba6bf3878d6.js"
 );
 
 workbox.clientsClaim();
